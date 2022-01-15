21630cca23faec726f726d8c23ea5192ee80ae92
Edgar9292 <92225390+Edgar9292@users.noreply.github.com>
2022-01-15 16:55:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+nome = input ('Escreva seu nome')
+idade = input ('Escreva sua idade')
+altura = input ('EScreva sua altura')
+endereço = input ('Escreva seu endereço')
+
+print (nome, 'tem', idade, 'anos e', altura, 'de altura', 'e é residente na', endereço)
